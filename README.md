411c0e168382ccb81aef8021976c5b1b6645137b
Miguel Camilo <miguelcamilo48@gmail.com>
2023-01-19 16:24:11 -0500

    switching from secrets.js to .env practice

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
 # MongoDB / Atlas Practice
 
-Practicing using MongoDB and creating a database with javascript and manipulating data
+Practicing using MongoDB and creating a database with javascript and manipulating data and using C.R.U.D
 
 ## Lesson Learned
 
@@ -8,5 +8,6 @@ Practicing using MongoDB and creating a database with javascript and manipulatin
 2. Atlas
 3. Javascript
 4. Databases
+5. CRUD
 
 ## See the the repo [click here.](https://github.com/MiguelCamilo/MongoDB-Practice-c9)
--- index.js
+++ index.js
@@ -1,50 +1,72 @@
-// conncect to mongoDB
-// import mongo library
-import { MongoClient } from "mongodb";
+// import library
+import { MongoClient, ObjectId } from "mongodb";
 
-// import credentials that allows us to connect to our server
-import { uri } from "./secrets.js";
+// this npm package is similar to secrets.js
+import dotenv from 'dotenv'
+dotenv.config()
 
-// connect to our mongo server
-const client = new MongoClient(uri);
+const client = new MongoClient(process.env.URI)
+const db = client.db(process.env.DB_NAME)
+const collection = db.collection('movies')
 
-// create a referrence to our database
-const db = client.db('mongo0')
-
-// add a document to the collection
-
-
-// add a piece of furniture to the collection
-const stool = {
-    name: 'Ergo Stool',
-    brand: 'Autonomous',
-    color: 'Evergreen',
-    price: 169.00,
-    warranty: '2 years'
+// create
+const movie1 = {
+    title: 'Interstellar',
+    rating: 'PG-13',
+    genre: 'Sci-Fi',
+    time: 'just right'
 }
 
-const chair = {
-    name: 'Ergo Chair',
-    brand: 'Autonomous',
-    color: 'Black',
-    price: 369.00,
-    warranty: '2 years'
+
+//! CRUD METHOD 
+
+//! CREATE:
+// adds a document 
+const addDoc = async item  => {
+    await collection.insertOne(item)
+        console.log('Added Document ', item)
 }
 
-const deskmat = {
-    name: 'Ergo Matt',
-    brand: 'Autonomous',
-    color: 'Grau',
-    price: 69.00,
-    warranty: '2 years'
+await addDoc(movie1)
+
+
+
+//! UPDATE
+const updateDoc = async (docId, fieldName, fieldValue) => {
+
+    const updateID = { _id: new ObjectId(docId)}  // updates the title
+    // the reason for the bracketes around fieldName is because we are passing a string as a parameter
+    const updateQuery = { $set: {[fieldName]: fieldValue}} // updates the title
+
+
+    // this func finds the data data by id and updates it
+    const result = await collection.findOneAndUpdate(updateID, updateQuery)
+
+}
+await updateDoc('63c999662f3e229d630a38fc','title','World of Wars')
+
+
+//! DELETE
+const deleteDoc = async (docID) => {
+
+    const deleteID = { _id: new ObjectId(docID)} // selects an ID
+    await collection.deleteOne(deleteID) // func to delete specific data 
+
 }
 
-// adds an object to the collection using async await function
-// async says don't move on until this line is complete
-const addOneItem = async (item) => {
-    await db.collection('items').insertOne() // add item
+await deleteDoc('63c999662f3e229d630a38fc')
+
+
+
+//! GET / RESPOND
+// getting information from the data base
+const getDocListing = async limit => {
+    // connect to db , find information , limit and place it in an array
+    const result = await collection.find({}).limit(limit).toArray()
+    console.table(result)
 }
 
-addOneItem(stool)
-addOneItem(chair)
-addOneItem(deskmat)
+await getDocListing(0) // the int allows you to limit the amount of data to see. 0 shows all.
+
+
+client.close() // closes connection
